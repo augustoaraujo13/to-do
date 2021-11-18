a435bc5913b4e5b93e4af7dacdbb85843041dabd
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-11-18 18:46:34 -0300

    css, bootstrap e js

--- todo/settings.py
+++ todo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
 ]
 
+#teste
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
