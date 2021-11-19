fb8922fc23ce36e5d6dd79ecb3a8bc9e2807a7b4
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-11-19 19:07:58 -0300

    tasks tempalte de task.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import task
 
+def taskList(request):
+    #faz um select na tabela tasks.
+    tasks = task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task1 = get_object_or_404(task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task1})
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
-
-
-def taskList(request):
-    return render(request, 'tasks/list.html')
-
 
 def yourName(request, name):
     return render(request, 'tasks/yourname.html', {'name': name})
