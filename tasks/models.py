2a592b426569a762390064d963ed9f3eeae2afb3
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-11-19 18:06:06 -0300

    django admin, models tasks, admin tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class task(models.Model):
+
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(
+        max_length=5,
+        choices=STATUS,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
