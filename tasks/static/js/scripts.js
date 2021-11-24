f701b35b6c1d1928d605eb1b9f4d6060d3938d83
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-11-24 17:44:16 -0300

    tasks aviso antes dd deleção

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,1 +1,20 @@
-console.log("Funcionou!")
+$( document ).ready(function() {
+
+    console.log("Testando")
+
+    var deleteBtn = $('.delete-btn');
+    
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault();
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+
+    });
+
+});
