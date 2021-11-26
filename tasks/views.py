68089c84cf1115b006839cce82fc32b641759b16
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-11-26 13:17:35 -0300

    projeto finalizado

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,8 @@ from .models import task
 from .forms import TaskForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+import datetime
+
 
 @login_required
 def taskList(request):
@@ -12,25 +14,33 @@ def taskList(request):
 
     search = request.GET.get('search')
     filter = request.GET.get('filter')
+    tasksDoneRecently = task.objects.filter(
+        done='done', updated__gt=datetime.datetime.now()-datetime.timedelta(days=30)).count()
+    tasksDone = task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = task.objects.filter(done='doing', user=request.user).count()
 
     if search:
         task1 = task.objects.filter(title__icontains=search, user=request.user)
-    
+
     elif filter:
         task1 = task.objects.filter(done=filter, user=request.user)
-    
+
     else:
         tasks_list = task.objects.all().order_by('-created').filter(user=request.user)
         paginator1 = Paginator(tasks_list, 5)
         page = request.GET.get('page')
         task1 = paginator1.get_page(page)
-        
-    return render(request, 'tasks/list.html', {'tasks': task1})
+
+    return render(request, 'tasks/list.html', {'tasks': task1, 
+    'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 
+    'tasksdoing': tasksDoing})
+
 
 @login_required
 def taskView(request, id):
     task1 = get_object_or_404(task, pk=id)
     return render(request, 'tasks/task.html', {'task': task1})
+
 
 @login_required
 def newTask(request):
@@ -46,6 +56,7 @@ def newTask(request):
     else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
+
 
 @login_required
 def editTask(request, id):
@@ -63,12 +74,14 @@ def editTask(request, id):
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task1})
 
+
 @login_required
 def deleteTask(request, id):
     task1 = get_object_or_404(task, pk=id)
     task1.delete()
     messages.info(request, 'Tarefa deletada com sucesso.')
     return redirect('/')
+
 
 @login_required
 def changeStatus(request, id):
@@ -87,6 +100,7 @@ def changeStatus(request, id):
 @login_required
 def helloWorld(request):
     return HttpResponse('Hello World!')
+
 
 @login_required
 def yourName(request, name):
